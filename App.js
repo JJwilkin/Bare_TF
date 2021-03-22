a69ba83a0fa13cf7ecae19296aaec725514c423e
Joshua Wilkinson <joshjeffreywilkinson@gmail.com>
2021-03-22 14:22:10 -0400

    test changes

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
 import { Camera } from 'expo-camera';
 import * as tf from '@tensorflow/tfjs';
 import {cameraWithTensors} from '@tensorflow/tfjs-react-native';
 import * as mobilenet from '@tensorflow-models/mobilenet';
 import * as knnClassifier from '@tensorflow-models/knn-classifier';
 import { makeObservable, observable, action, computed } from "mobx"
-const customModel = require('./model_3.json');
 import TextDisplay from './TextDisplay';
-// const Tensorset = require('tensorset');
+import { RNCamera } from 'react-native-camera';
+import ExampleCamera from './ExampleCamera';
+
+const customModel = require('./model_3.json');
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 class WordPrediction {
   word = ""
@@ -25,7 +29,6 @@ class WordPrediction {
       })
   }
   get getWord () {
-    // console.log(this.word)
     return this.word
   }
   
@@ -35,7 +38,6 @@ class WordPrediction {
 
   toggle(word) {
       this.word = word
-      // console.log(word)
   }
 
   prediction(val) {
@@ -58,14 +60,7 @@ export default function App() {
   const tensorDims = { width: 152, height: 200 }; 
 
   async function load(classifier) {
-      //can be change to other source
-   
-    // console.log(customModel)
     let tensorObj = parse(customModel);
-    //covert back to tensor
-    // Object.keys(tensorObj).forEach((key) => {
-    //   tensorObj[key] = tf.tensor(tensorObj[key], [tensorObj[key].length / 1000, 1000])
-    // })
     await classifier.setClassifierDataset(tensorObj);
   }
 
@@ -79,42 +74,41 @@ export default function App() {
 }
 
   let requestAnimationFrameId = 0;
-  useEffect(() => {
-    if(!frameworkReady) {
-      (async () => {
+  // useEffect(() => {
+  //   if(!frameworkReady) {
+  //     (async () => {
         
-        //check permissions
-        const { status } = await Camera.requestPermissionsAsync();
-        console.log(`permissions status: ${status}`);
-        setHasPermission(status === 'granted');
+  //       //check permissions
+  //       const { status } = await Camera.requestPermissionsAsync();
+  //       console.log(`permissions status: ${status}`);
+  //       setHasPermission(status === 'granted');
 
-        //we must always wait for the Tensorflow API to be ready before any TF operation...
-        await tf.ready();
+  //       //we must always wait for the Tensorflow API to be ready before any TF operation...
+  //       await tf.ready();
 
-        // const model = await mobilenet.load()
-        // console.log(model)
-        //load the mobilenet model and save it in state
-        setMobilenetModel(await loadMobileNetModel());
-        let classifier = knnClassifier.create();
-        await load(classifier);
-        // let existingJson = await (await fetch('./model.json')).json()
-        // if (existingJson) {
-        //   let dataset = await Tensorset.parse(existingJson);
-        //   classifier.setClassifierDataset(dataset);
-        //   let classes = Object.keys(classifier.getClassExampleCount());
-        //   console.log(classes);
-        // }
-        setClassifier(classifier);
-        setFrameworkReady(true);
-      })();
-    }
-  }, [frameworkReady]);
+  //       //load the mobilenet model and save it in state
+  //       setMobilenetModel(await loadMobileNetModel());
+  //       let classifier = knnClassifier.create();
+  //       await load(classifier);
+  //       // let existingJson = await (await fetch('./model.json')).json()
+  //       // if (existingJson) {
+  //       //   let dataset = await Tensorset.parse(existingJson);
+  //       //   classifier.setClassifierDataset(dataset);
+  //       //   let classes = Object.keys(classifier.getClassExampleCount());
+  //       //   console.log(classes);
+  //       // }
+  //       setClassifier(classifier);
+  //       setFrameworkReady(true);
 
-  useEffect(() => {
-    return () => {
-      cancelAnimationFrame(requestAnimationFrameId);
-    };
-  }, [requestAnimationFrameId]);
+  //     })();
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   return () => {
+  //     cancelAnimationFrame(requestAnimationFrameId);
+  //   };
+  // }, [requestAnimationFrameId]);
 
   const loadMobileNetModel = async () => {
     const model = await mobilenet.load();
@@ -140,47 +134,22 @@ export default function App() {
   }
 
   const getPrediction = async (tensor) => {
-    // console.log(store.getShowPrediction);
     if ((!store.getShowPrediction) || (!tensor && tensor != null) ) return;
 
-    // setTimeout(()=>{store.prediction(false)},500);
     const activation = mobilenetModel.infer(tensor, 'conv_preds');
-          // Get the most likely class and confidence from the classifier module.
+    // Get the most likely class and confidence from the classifier module.
       const result = await classifier.predictClass(activation);
       const prediction = `${result.label} ${result.confidences[result.label]}`
       store.toggle(prediction);
-    //topk set to 1
-    // const prediction = await mobilenetModel.classify(tensor, 1);
-    // console.log(`prediction: ${JSON.stringify(prediction)}`);
-
-    // if(!prediction || prediction.length === 0) { return; }
     
-    // //only attempt translation when confidence is higher than 20%
-    // if(prediction[0].probability > 0.4) {
-
-    //   //stop looping!
-    //   // cancelAnimationFrame(requestAnimationFrameId);
-    //   // setPredictionFound(true);
-    //   // setWord(prediction[0].className);
-    //   // store.toggle(prediction[0].className);
-    //   // console.log(store.word)
-    //   //get translation!
-    //   // await getTranslation(prediction[0].className);
-    // }
   }
 
 const handleCameraStream = (imageAsTensors) => {
     const loop = async () => {
-      
-      // if ( Platform.OS !== "ios" && Math.random()*50 % 50 == 1) run = false;
-      // else if( Math.random()*7 % 7 == 0 ) { run = false; }
         const nextImageTensor = await imageAsTensors.next().value;
         await getPrediction(nextImageTensor);
         requestAnimationFrameId = requestAnimationFrame(loop);
-        // console.log('hi')
       }
-      
-    
     if(!predictionFound) loop();
   }
 
@@ -191,18 +160,13 @@ const handleCameraStream = (imageAsTensors) => {
     return <Text>No access to camera</Text>;
   }
   return (
-    <View style={styles.container}>
-      {/* <View style={styles.header}>
-        <Text style={styles.title}>
-          {word}
-        </Text>
-      </View> */}
-      <TextDisplay store={store} styles={styles}/>
-      <View style={styles.body}>
-        { frameworkReady ? renderCameraView() : <Text styles={styles.title}>Loading</Text> }
-      </View>  
-    </View>
-    
+    // <View style={styles.container}>
+    //   <TextDisplay store={store} styles={styles} frameworkReady={frameworkReady}/>
+    //   <View style={styles.body}>
+    //     { frameworkReady ? renderCameraView() : <Text styles={styles.title}>Loading</Text> }
+    //   </View>  
+    // </View>
+    <ExampleCamera/>
   );
 }
 
@@ -214,18 +178,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#E8E8E8',
   },
   header: {
-    backgroundColor: '#41005d'
+    
   },
   title: {
-    margin: 10,
+    // margin: 10,
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
-    color: '#ffffff'
+    color: '#ffffff',
+    zIndex:2
   },
   body: {
-    padding: 5,
-    paddingTop: 25
+    padding: 0,
   },
   cameraView: {
     display: 'flex',
@@ -238,8 +202,8 @@ const styles = StyleSheet.create({
     paddingTop: 10
   },
   camera : {
-    width: 700/2,
-    height: 800/2,
+    width: windowWidth,
+    height: windowHeight,
     zIndex: 1,
     borderWidth: 0,
     borderRadius: 0,
--- TextDisplay.js
+++ TextDisplay.js
@@ -13,83 +13,48 @@ import {
 import { DeviceMotion } from 'expo-sensors';
 
 export default function TextDisplay(props) {
-  const { store, styles } = props;
+  const { store, styles, frameworkReady } = props;
   const [word, setWord] = useState("");
-  const [motionData, setMotion] = useState();
-  const [subscription, setSubScription] = useState();
+  
   useEffect(()=>{
-    (async () => {
-      // const status = await Permissions.askAsync(Permissions.MOTION);
-      subscribe();
-    })();
-    
-  },[]);
+    if (frameworkReady){
+      (async () => {
+        subscribe();
+      })();
+    }
+  },[frameworkReady]);
+
   useEffect(()=>{
     return () => unsubscribe();
   },[])
 
   const subscribe = () => {
+    console.log("Subscribing")
+  
     DeviceMotion.setUpdateInterval(1000)
     DeviceMotion.addListener(motionData => {
       const {x, y, z} = motionData.acceleration;
-      // console.log(Math.abs(x)< 0.3)
-      if (!store.getShowPrediction && Math.abs(x)< 0.08 && Math.abs(y) < 0.08 && Math.abs(z) < 0.08){
-        // console.log(motionData)
+      if (Math.abs(x)< 0.08 && Math.abs(y) < 0.08 && Math.abs(z) < 0.08){
         store.prediction(true)
     
       }
       else {
         store.prediction(false);
       }
-      
-      
     })
-        
-
-    // setSubScription(tempSub);
   }
 
   const unsubscribe = () => {
-    // subscription.remove();
     DeviceMotion.removeAllListeners();
   }
-  
-
-  // setInterval(() => {
-  //   store.prediction(true);
-  //   setWord(store.getWord);
-  //   setTimeout(() => {
-  //     store.prediction(false);
-  //   }, 200);
-  // }, 1000);
-
-  // setInterval(() => {
-  //   let currentPrediction = store.getWord;
-
-  //   setTimeout(()=>{
-  //     if (currentPrediction = store.getWord) {
-  //       store.toggle("");
-  //     }
-  //   }, 1500)
-
-  // }, 2500);
 
   setInterval(()=>{
     setWord(store.word)
   },1000)
 
-  // const triggerPrediction = () => {
-  //   store.prediction(true);
-  //   setTimeout(() => {
-  //     setWord(store.word);
-  //     store.prediction(false);
-  //   }, 1500)
-  // }
   return (
     <View style={styles.header}>
-      
-      {/* <Button title="Get Prediction" onPress={() => setWord(store.getWord)}/> */}
-      <Text style={styles.title}>{store.getWord}</Text>
+      {/* <Text style={styles.title}>{word}</Text> */}
     </View>
   );
 }
